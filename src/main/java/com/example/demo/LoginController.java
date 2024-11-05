3223f465ecfe2e17546e53d889be27ca3052807a
João Pedro de Brito Matias <116584601+Sansaocarrasco@users.noreply.github.com>
2024-11-04 23:19:02 -0300

    Login Screen

--- src/main/java/com/example/demo/LoginController.java
+++ src/main/java/com/example/demo/LoginController.java
@@ -2,25 +2,43 @@ package com.example.demo;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
-public class LoginController {
+import java.io.File;
+import java.util.ResourceBundle;
+
+import java.net.URL;
+
+public class LoginController implements Initializable {
 
     @FXML
+    private Label loginMessageLabel;
+    @FXML
     private Button cancelButton;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private ImageView brandingImageView;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        File brandingFile = new File("Images/logo.png");
+        Image brandingImage = new Image(brandingFile.toURI().toString());
+        brandingImageView.setImage(brandingImage);
+    };
+
+    public void loginButtonAction(ActionEvent event) {
+        loginMessageLabel.setText("Tente Novamente!");
+    }
 
     public void cancelButtonAction(ActionEvent event) {
         Stage stage = (Stage) cancelButton.getScene().getWindow();
         stage.close();
-    }
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
     }
 
     public void loginButtonOnAction(ActionEvent actionEvent) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
+    requires java.desktop;
 
 
     opens com.example.demo to javafx.fxml;
